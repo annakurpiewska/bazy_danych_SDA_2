0ccfef2eacde7b88db03b798eeaaf8082ac18ca1
rdkkz <Bolszewo11>
2020-08-02 09:47:42 +0200

    first commit

--- 1.py
+++ 1.py
@@ -17,19 +17,32 @@ class User(base):
     def __repr__(self):
         return f"User(name={self.name}, fullname={self.fullname}, nickname={self.nickname})"
 
-base.metadata.create_all(engine)
+    # def __str__(self):
+    #     return('test')
+
+base.metadata.create_all(engine) # wrzuć tabele do bazy danych
 
 
 ed_user = User(name='ed', fullname='ed jones', nickname='ednickname')
 print(ed_user.name)
 
-sesion = sessionmaker(bind=engine)
+session = sessionmaker(bind=engine)
 
-session1 = sesion()
+session1 = session()
 session1.add(ed_user)
 
-our_user = session1.query(User).filter_by(name='ed').first()
-print(our_user)
+
 
 session1.commit()
 session1.close()
+
+
+
+session2 = session()
+
+our_user = session1.query(User).filter_by(name='ed').first()
+print(our_user)
+print(f"id > {our_user.id}  name > {our_user.name}  fullname > {our_user.fullname}")
+
+session2.commit()
+session2.close()
